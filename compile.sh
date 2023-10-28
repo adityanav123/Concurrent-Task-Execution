7cfe407a0d049652fc403893526629cee65de701
Aditya Navphule <adityanavphule122@gmail.com>
2023-10-28 18:01:02 +0530

    Updated Compile.sh

--- compile.sh
+++ compile.sh
@@ -1,17 +1,3 @@
 #!/bin/bash
 
-filename=$1
-extra_args=$3
-compiler_to_be_used=$2
-
-if [[ "$compiler_to_be_used" == "" ]]; then
-    compiler_to_be_used="clang++"
-fi
-
-filename_without_extension="${filename%.*}"
-
-if [[ "$extra_args" != "" ]]; then
-    $compiler_to_be_used -Wall -std=c++20 $filename $extra_args -o $filename_without_extension
-else
-    $compiler_to_be_used -Wall -std=c++20 $filename -o $filename_without_extension
-fi
+g++ -Wall Main.cpp task.cpp TaskScheduler.cpp -lpthread -o Executable
